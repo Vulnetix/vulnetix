e6032ce9547ecd873c4da2d69d56e4c863a4c1fe
0x73746F66 <github.chris@trivialsec.com>
2024-06-14 23:37:19 +1000

    feat: refactor GitHub for caching

--- functions/github/repos.js
+++ functions/github/repos.js
@@ -1,3 +1,7 @@
+import { CloudFlare, GitHub } from "../../src/utils"
+
+const cf = CloudFlare()
+
 export async function onRequestGet(context) {
     const {
         request, // same as existing Worker API
@@ -25,22 +29,56 @@ export async function onRequestGet(context) {
         return Response.json({ 'err': 'Expired' })
     }
     try {
-        const { results } = await env.d1db.prepare("SELECT * FROM github_apps WHERE memberEmail = ?")
-            .bind(session.memberEmail)
-            .all()
-
-        let installs = []
-        for (const github_app of results) {
-            if (!github_app.accessToken) {
-                console.log(`github_apps kid=${token} installationId=${github_app.installationId}`)
+        const oneDayAgo = new Date(Date.now() - 24 * 60 * 60 * 1000);
+        const githubApps = cf.d1all("d1db", "SELECT * FROM github_apps WHERE memberEmail = ?", session.memberEmail)
+        const installs = []
+        for (const app of githubApps) {
+            if (!app.accessToken) {
+                console.log(`github_apps kid=${token} installationId=${app.installationId}`)
                 throw new Error('github_apps invalid')
             }
-            const fetcher = new GitHubRepoFetcher(github_app.accessToken)
+            const gh = new GitHub(app.accessToken)
+            const prefixRepos = `/github/${app.installationId}/repos/`
+            let repoCache = cf.r2list('r2icache', prefixRepos)
 
+            const repos = []
+            for (const repo of await gh.getRepos()) {
+                const pathSuffix = `${repo.full_name}/${repo.id}.json`
+                const repoMetadata = repoCache.filter(r => r.key.endsWith(pathSuffix))
+                if (repoMetadata.length === 0) {
+                    await cf.r2put('r2icache', `${prefixRepos}${pathSuffix}`, repo)
+                }
+                const data = {
+                    ghid: repo.id,
+                    fullName: repo.full_name,
+                    createdAt: repo.created_at,
+                    visibility: repo.visibility,
+                    archived: repo.archived,
+                    defaultBranch: repo.default_branch,
+                    pushedAt: repo.pushed_at,
+                    avatarUrl: repo.owner.avatar_url,
+                    license: repo.license,
+                }
+                const prefixBranches = `/github/${app.installationId}/branches/${repo.full_name}/`
+                data.branch = repo.default_branch
+                const branchCache = cf.r2get('r2icache', `${prefixBranches}${repo.default_branch}.json`)
+                if (branchCache) {
+                    const branch = branchCache.json()
+                    data.latestCommitSHA = branch?.commit?.sha
+                    data.latestCommitMessage = branch?.commit?.message
+                    data.latestCommitVerification = branch?.commit?.verification
+                    data.latestCommitter = branch?.commit?.committer
+                    data.latestStats = branch?.stats
+                    data.latestFilesChanged = branch?.files?.length
+                    data.dotfileExists = branch?.exists
+                    data.dotfileContents = branch?.content
+                }
+                repos.push(data)
+            }
             installs = installs.concat({
-                repos: await fetcher.getRepoDetails(),
-                installationId: github_app.installationId,
-                created: github_app.created,
+                repos,
+                installationId: app.installationId,
+                created: app.created,
             })
         }
 
@@ -49,108 +87,5 @@ export async function onRequestGet(context) {
         console.error(e)
 
         return Response.json(e)
-    }
-}
-
-class GitHubRepoFetcher {
-    constructor(accessToken) {
-        this.headers = {
-            'Accept': 'application/vnd.github+json',
-            'Authorization': `token ${accessToken}`,
-            'X-GitHub-Api-Version': '2022-11-28',
-            'User-Agent': 'Triage-by-Trivial-Security',
-        }
-        this.baseUrl = "https://api.github.com"
-    }
-    async fetchJSON(url) {
-        console.log(url)
-
-        const response = await fetch(url, { headers: this.headers })
-        if (!response.ok) {
-            console.error(await response.text(), response.headers.entries().map(pair => `${pair[0]}: ${pair[1]}`))
-            throw new Error(`GitHubRepoFetcher error! status: ${response.status} ${response.statusText}`)
-        }
-
-        return response.json()
-    }
-    async getRepos() {
-        // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
-        return await this.fetchJSON(`${this.baseUrl}/user/repos`)
-    }
-    async getBranch(repo, branch) {
-        // https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#get-a-branch
-        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/branches/${branch}`)
-    }
-    async getBranches(repo) {
-        // https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#list-branches
-        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/branches`)
-    }
-    async getCommit(repo, branch) {
-        // https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
-        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/commits/${branch.commit.sha}`)
-    }
-    async getFileContents(repo, branch) {
-        // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
-        const fileUrl = `${this.baseUrl}/repos/${repo.full_name}/contents/.trivialsec?ref=${branch.name}`
-
-        console.log(fileUrl)
-        try {
-            const fileResponse = await fetch(fileUrl, { headers: this.headers })
-            if (!fileResponse.ok) {
-                if (fileResponse.status === 404) {
-                    return { exists: false, content: null }
-                }
-                console.error(await response.text(), response.headers.entries().map(pair => `${pair[0]}: ${pair[1]}`))
-                throw new Error(`getFileContents error! status: ${response.status} ${response.statusText}`)
-            }
-            const file = await fileResponse.json()
-            const content = Buffer.from(file.content, file.encoding).toString('utf-8')
-
-            return { exists: true, content }
-        } catch (error) {
-            console.error(error)
-
-            return { exists: false, content: null }
-        }
-    }
-    async getRepoDetails() {
-        const collection = []
-        for (const repo of await this.getRepos()) {
-            const data = {
-                ghid: repo.id,
-                fullName: repo.full_name,
-                createdAt: repo.created_at,
-                visibility: repo.visibility,
-                archived: repo.archived,
-                defaultBranch: repo.default_branch,
-                pushedAt: repo.pushed_at,
-                avatarUrl: repo.owner.avatar_url,
-                license: repo.license,
-            }
-
-            for (const branch of await this.getBranches(repo)) {
-                data.branch = branch.name
-                data.latestCommitSHA = branch.commit.sha
-
-                const branchData = Object.assign({}, data)
-                if (repo.default_branch === branch.name) {
-                    const latestCommit = await this.getCommit(repo, branch)
-
-                    branchData.latestCommitMessage = latestCommit.commit.message
-                    branchData.latestCommitVerification = latestCommit.commit.verification
-                    branchData.latestCommitter = latestCommit.commit.committer
-                    branchData.latestStats = latestCommit.stats
-                    branchData.latestFilesChanged = latestCommit.files.length
-
-                    const fileDetails = await this.getFileContents(repo, branch)
-
-                    branchData.dotfileExists = fileDetails.exists
-                    branchData.dotfileContents = fileDetails.content
-                }
-                collection.push(branchData)
-            }
-        }
-
-        return collection
     }
 }
--- src/utils.js
+++ src/utils.js
@@ -235,3 +235,110 @@ export const octodex = [
     "original.png",
     "idokungfoo-avatar.jpg"
 ]
+
+class CloudFlare {
+    constructor() {
+
+    }
+    async d1all(db, sql, bind) {
+        const { results } = await env[db].prepare(sql).bind(bind).all()
+        return results
+    }
+    async r2get(db, objectKey, uploadedAfter) {
+        const options = {
+            conditional: {
+                uploadedAfter
+            },
+        }
+
+        return await env[db].get(objectKey, options)
+    }
+    async r2list(db, prefix = '/', limit = 1000) {
+        const options = {
+            limit,
+            prefix,
+            include: ['customMetadata'],
+        }
+
+        const listed = await env[db].list(options)
+
+        let { truncated } = listed
+        let cursor = truncated ? listed.cursor : undefined
+
+        while (truncated) {
+            const next = await env[db].list({
+                ...options,
+                cursor: cursor,
+            })
+            listed.objects.push(...next.objects)
+
+            truncated = next.truncated
+            cursor = next.cursor
+        }
+
+        return listed.objects
+    }
+}
+
+class GitHub {
+    constructor(accessToken) {
+        this.headers = {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': `token ${accessToken}`,
+            'X-GitHub-Api-Version': '2022-11-28',
+            'User-Agent': 'Triage-by-Trivial-Security',
+        }
+        this.baseUrl = "https://api.github.com"
+    }
+    async fetchJSON(url) {
+        console.log(url)
+
+        const response = await fetch(url, { headers: this.headers })
+        if (!response.ok) {
+            console.error(await response.text(), response.headers.entries().map(pair => `${pair[0]}: ${pair[1]}`))
+            throw new Error(`GitHub error! status: ${response.status} ${response.statusText}`)
+        }
+
+        return response.json()
+    }
+    async getRepos() {
+        // https://docs.github.com/en/rest/repos/repos?apiVersion=2022-11-28#list-repositories-for-the-authenticated-user
+        return await this.fetchJSON(`${this.baseUrl}/user/repos`)
+    }
+    async getBranch(repo, branch) {
+        // https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#get-a-branch
+        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/branches/${branch}`)
+    }
+    async getBranches(repo) {
+        // https://docs.github.com/en/rest/branches/branches?apiVersion=2022-11-28#list-branches
+        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/branches`)
+    }
+    async getCommit(repo, branch) {
+        // https://docs.github.com/en/rest/commits/commits?apiVersion=2022-11-28
+        return await this.fetchJSON(`${this.baseUrl}/repos/${repo.full_name}/commits/${branch.commit.sha}`)
+    }
+    async getFileContents(repo, branch) {
+        // https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28
+        const fileUrl = `${this.baseUrl}/repos/${repo.full_name}/contents/.trivialsec?ref=${branch.name}`
+
+        console.log(fileUrl)
+        try {
+            const fileResponse = await fetch(fileUrl, { headers: this.headers })
+            if (!fileResponse.ok) {
+                if (fileResponse.status === 404) {
+                    return { exists: false, content: null }
+                }
+                console.error(await response.text(), response.headers.entries().map(pair => `${pair[0]}: ${pair[1]}`))
+                throw new Error(`getFileContents error! status: ${response.status} ${response.statusText}`)
+            }
+            const file = await fileResponse.json()
+            const content = Buffer.from(file.content, file.encoding).toString('utf-8')
+
+            return { exists: true, content }
+        } catch (error) {
+            console.error(error)
+
+            return { exists: false, content: null }
+        }
+    }
+}
