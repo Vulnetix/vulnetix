1337a816db0d00bd151b9bcd985dd486e6f28fda
0x73746F66 <github.chris@trivialsec.com>
2024-06-16 03:44:21 +1000

    fix: INSERT OR REPLACE

--- functions/github/repos.js
+++ functions/github/repos.js
@@ -67,7 +67,7 @@ export async function onRequestGet(context) {
                     licenseSpdxId: repo.license?.spdx_id || '',
                     licenseName: repo.license?.name || '',
                 }
-                const info = await env.d1db.prepare('INSERT INTO git_repos (pk, fullName, createdAt, updatedAt, pushedAt, defaultBranch, ownerId, memberEmail, licenseSpdxId, licenseName, fork, template, archived, visibility, avatarUrl) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15)')
+                const info = await env.d1db.prepare('INSERT OR REPLACE INTO git_repos (pk, fullName, createdAt, updatedAt, pushedAt, defaultBranch, ownerId, memberEmail, licenseSpdxId, licenseName, fork, template, archived, visibility, avatarUrl) VALUES (?1, ?2, ?3, ?4, ?5, ?6, ?7, ?8, ?9, ?10, ?11, ?12, ?13, ?14, ?15)')
                     .bind(data.ghid, data.fullName, data.createdAt, data.updatedAt, data.pushedAt, data.defaultBranch, data.ownerId, session.memberEmail, data.licenseSpdxId, data.licenseName, data.fork, data.template, data.archived, data.visibility, data.avatarUrl)
                     .run()
                 console.log(`/github/repos git_repos ${data.fullName} kid=${token}`, info)
