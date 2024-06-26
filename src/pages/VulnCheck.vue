1049e035d10c247b908a261b819a5134ca17406e
0x73746F66 <github.chris@trivialsec.com>
2024-06-26 15:50:32 +1000

    started VulnCheck integration
    Signed-off-by: 0x73746F66 <github.chris@trivialsec.com>

--- src/pages/VulnCheck.vue
+++ src/pages/VulnCheck.vue
@@ -1,53 +1,218 @@
 <script setup>
-import DemoSimpleTableBasics from '@/views/pages/tables/DemoSimpleTableBasics.vue'
-import DemoSimpleTableDensity from '@/views/pages/tables/DemoSimpleTableDensity.vue'
-import DemoSimpleTableFixedHeader from '@/views/pages/tables/DemoSimpleTableFixedHeader.vue'
-import DemoSimpleTableHeight from '@/views/pages/tables/DemoSimpleTableHeight.vue'
-import DemoSimpleTableTheme from '@/views/pages/tables/DemoSimpleTableTheme.vue'
+import { default as axios } from 'axios';
+import { reactive } from 'vue';
+import { useTheme } from 'vuetify';
+import router from "../router";
+
+const { global } = useTheme()
+
+const initialState = {
+    error: "",
+    warning: "",
+    success: "",
+    info: "",
+    loading: false,
+    apiKey: '',
+    log: []
+}
+const state = reactive({
+    ...initialState,
+})
+axios.defaults.headers.common = {
+    'x-trivialsec': localStorage.getItem('/session/token') || '',
+}
+const clearAlerts = () => {
+    state.error = ''
+    state.warning = ''
+    state.success = ''
+    state.info = ''
+}
+class VulnCheck {
+    constructor() {
+        this.refresh()
+    }
+    refresh = async () => {
+
+    }
+    save = async device => {
+        clearAlerts()
+        state.loading = true
+        try {
+            const member = {}
+            member[device.key] = (device.email ? 1 << 2 : 0) |
+                (device.browser ? 1 << 1 : 0) |
+                (device.webhook ? 1 << 0 : 0)
+
+            const { data } = await axios.post(`/me`, member, { headers: { 'Content-Type': 'application/json' } })
+            state.loading = false
+
+            if (typeof data === "string" && !isJSON(data)) {
+                state.error = "Profile data could not be saved, please try again later."
+
+                return
+            }
+            if (data?.err) {
+                state.error = data.err
+            }
+            if (["Expired", "Revoked", "Forbidden"].includes(data?.result)) {
+                state.info = data.result
+
+                return setTimeout(router.push('/logout'), 2000)
+            }
+            if (data.ok === true) {
+                state.member[device.key] = member[device.key]
+                localStorage.setItem(`/member/${device.key}`, member[device.key])
+            } else {
+                state.info = data?.result || 'No change'
+            }
+
+            return
+        } catch (e) {
+            console.error(e)
+            state.error = typeof e === "string" ? e : `${e.code} ${e.message}`
+            state.loading = false
+        }
+    }
+}
+
+const vulncheck = reactive(new VulnCheck())
 </script>
 
 <template>
-  <VRow>
-    <VCol cols="12">
-      <VCard title="Basic">
-        <DemoSimpleTableBasics />
-      </VCard>
-    </VCol>
+    <Vrow>
+        <VCol cols="12">
+            <VAlert
+                v-if="state.error"
+                color="error"
+                icon="$error"
+                title="Error"
+                :text="state.error"
+                border="start"
+                variant="tonal"
+            />
+            <VAlert
+                v-if="state.warning"
+                color="warning"
+                icon="$warning"
+                title="Warning"
+                :text="state.warning"
+                border="start"
+                variant="tonal"
+            />
+            <VAlert
+                v-if="state.success"
+                color="success"
+                icon="$success"
+                title="Success"
+                :text="state.success"
+                border="start"
+                variant="tonal"
+            />
+            <VAlert
+                v-if="state.info"
+                color="info"
+                icon="$info"
+                title="Information"
+                :text="state.info"
+                border="start"
+                variant="tonal"
+            />
+        </VCol>
+    </Vrow>
+    <VCard title="VulnCheck Integration">
+        <VAlert
+            v-if="!state.apiKey"
+            color="info"
+            icon="$info"
+            title="Information"
+            border="start"
+            variant="tonal"
+            closable
+        >
+            Create a FREE token to use the VulnCheck API, you can
+            <a
+                :class="global.name.value === 'dark' ? 'text-secondary' : 'text-primary'"
+                href="https://vulncheck.com/settings/token/newtoken"
+                target="_blank"
+            >create API Tokens here.</a>
+        </VAlert>
 
-    <VCol cols="12">
-      <VCard title="Theme">
         <VCardText>
-          use <code>theme</code> prop to switch table to the dark theme.
+            <VForm @submit.prevent="() => { }">
+                <p class="text-base font-weight-medium">
+                    Request vulnerabilities related to a CPE (Common Platform Enumeration) URI string, or Package URL
+                    Scheme (PURL)
+                </p>
+                <VRow>
+                    <VCol
+                        md="6"
+                        cols="12"
+                    >
+                        <VTextField
+                            :disabled="state.loading"
+                            v-model="state.apiKey"
+                            placeholder="vulncheck_xxxx...xxxx"
+                            label="VulnCheck API Token"
+                        />
+                    </VCol>
+                </VRow>
+            </VForm>
         </VCardText>
-        <DemoSimpleTableTheme />
-      </VCard>
-    </VCol>
 
-    <VCol cols="12">
-      <VCard title="Density">
-        <VCardText>
-          You can show a dense version of the table by using the <code>density</code> prop.
-        </VCardText>
-        <DemoSimpleTableDensity />
-      </VCard>
-    </VCol>
-
-    <VCol cols="12">
-      <VCard title="Height">
-        <VCardText>
-          You can set the height of the table by using the <code>height</code> prop.
-        </VCardText>
-        <DemoSimpleTableHeight />
-      </VCard>
-    </VCol>
-
-    <VCol cols="12">
-      <VCard title="Fixed Header">
-        <VCardText>
-          You can fix the header of table by using the <code>fixed-header</code> prop.
-        </VCardText>
-        <DemoSimpleTableFixedHeader />
-      </VCard>
-    </VCol>
-  </VRow>
+        <VTable class="text-no-wrap">
+            <thead>
+                <tr>
+                    <th scope="col">
+                        Event Type
+                    </th>
+                    <th scope="col">
+                        Email
+                    </th>
+                    <th scope="col">
+                        Browser
+                    </th>
+                    <th scope="col">
+                        Webhook
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="(device, i) in recentDevices"
+                    :key="i"
+                >
+                    <td>
+                        {{ device.type }}
+                    </td>
+                    <td>
+                        <VCheckbox
+                            v-model="device.email"
+                            @change="profile.save(device)"
+                        />
+                    </td>
+                    <td>
+                        <VCheckbox
+                            v-model="device.browser"
+                            @change="profile.save(device)"
+                        />
+                    </td>
+                    <td>
+                        <VCheckbox
+                            v-model="device.webhook"
+                            @change="profile.save(device)"
+                        />
+                    </td>
+                </tr>
+            </tbody>
+        </VTable>
+        <VDivider />
+    </VCard>
 </template>
+
+<style lang="scss" scoped>
+.v-table {
+    th {
+        text-align: start !important;
+    }
+}
+</style>
