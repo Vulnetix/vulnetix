b11b566870ad2c76421298c3bcac0cb71f7ffaaf
Christopher Langton <chris@langton.cloud>
2024-09-20 22:46:54 +1000

    feat: moved to 0.2.0 spec for TEA

--- functions/v0.2.0/product/[tei].js
+++ functions/v0.2.0/product/[tei].js
@@ -1,8 +1,14 @@
+import { Visibility, Product } from "@/tea";
 import { PrismaD1 } from '@prisma/adapter-d1';
 import { PrismaClient } from '@prisma/client';
 
 /**
- * Retrieve a list of BOM serial numbers and versions that match the supplied metadata component search criteria.
+ * If only the serialNumber parameter is supplied, retrieve the latest version of the BOM from the repository.
+ * If providing serialNumber and version, a specific version of the BOM will be retrieved.
+ * Supports HTTP content negotiation for all CycloneDX BOM formats and versions.
+ * If original is true, returns the original, unmodified BOM.
+ * 
+ * curl -X GET 'http://localhost:8000/v1/bom?serialNumber=urn%3Auuid%3A3e671687-395b-41f5-a30f-a58921a69b79' -H 'accept: application/vnd.cyclonedx+json; version=1.4'
  */
 export async function onRequestGet(context) {
     const {
@@ -22,15 +28,19 @@ export async function onRequestGet(context) {
                 timeout: 2000, // default: 5000
             },
         })
-        params.group
-        params.name
-        params.version
+        params.tei // TEI unique product index
+        const { searchParams } = new URL(request.url)
+        const visibility = searchParams.get('visibility') // Used to specify whether we list public or private components
+        if (visibility && ![Visibility.ALLAVAILABLE, Visibility.PUBLICONLY].includes(visibility)) {
+            return Response(null, { status: 422, statusText: `Invalid value provided: visibility=${visibility}` })
+        }
+
         // const member = await prisma.members.findFirst({
         //     where: {
         //         email: verificationResult.session.memberEmail,
         //     },
         // })
-        // return Response.json({ ok: true, member })
+        // return Response.json([]) // [ Product ]
     } catch (err) {
         console.error(err)
         // return Response.json({ ok: false, error: { message: err }, result: AuthResult.REVOKED })
