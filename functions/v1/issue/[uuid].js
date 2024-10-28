2143049c2d530e0d2fb9b45f99120e55a80bc4c1
Christopher Langton <chris@langton.cloud>
2024-10-28 14:39:24 +1100

    fix: triage update

--- functions/v1/issue/[uuid].js
+++ functions/v1/issue/[uuid].js
@@ -206,7 +206,7 @@ export async function onRequestGet(context) {
         if (seen === 1) {
             seenAt = new Date().getTime()
         }
-        const vexExist = finding.triage.some(t => t.analysisState === analysisState).length !== 0
+        const vexExist = finding.triage.filter(t => t.analysisState === analysisState).length !== 0
         let vexData = finding.triage.filter(t => t.analysisState === analysisState).pop() || {}
         if (!vexExist) {
             vexData.analysisState = analysisState
