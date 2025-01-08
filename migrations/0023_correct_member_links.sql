465d3793a278f2db588866717883217733c35e67
Christopher Langton <chris@langton.cloud>
2025-01-08 21:59:39 +1100

    feat: Showing artifacts for issue. And animated Pix

--- migrations/0023_correct_member_links.sql
+++ migrations/0023_correct_member_links.sql
@@ -1,88 +1,88 @@
 PRAGMA defer_foreign_keys=ON;
 PRAGMA foreign_keys=OFF;
 
--- ALTER TABLE IntegrationUsageLog RENAME TO IntegrationUsageLog2;
--- CREATE TABLE "IntegrationUsageLog" (
---     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
---     "orgId" TEXT NOT NULL,
---     "source" TEXT NOT NULL,
---     "memberUuid" TEXT NOT NULL,
---     "memberEmail" TEXT NOT NULL,
---     "request" TEXT NOT NULL,
---     "response" TEXT,
---     "statusCode" INTEGER NOT NULL,
---     "createdAt" INTEGER NOT NULL
--- );
--- INSERT INTO "IntegrationUsageLog" ("memberUuid", "memberEmail", "createdAt", "id", "orgId", "request", "response", "source", "statusCode") SELECT "" AS "memberUuid", "memberEmail", "createdAt", "id", "orgId", "request", "response", "source", "statusCode" FROM "IntegrationUsageLog2";
+ALTER TABLE IntegrationUsageLog RENAME TO IntegrationUsageLog2;
+CREATE TABLE "IntegrationUsageLog" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "orgId" TEXT NOT NULL,
+    "source" TEXT NOT NULL,
+    "memberUuid" TEXT NOT NULL,
+    "memberEmail" TEXT NOT NULL,
+    "request" TEXT NOT NULL,
+    "response" TEXT,
+    "statusCode" INTEGER NOT NULL,
+    "createdAt" INTEGER NOT NULL
+);
+INSERT INTO "IntegrationUsageLog" ("memberUuid", "memberEmail", "createdAt", "id", "orgId", "request", "response", "source", "statusCode") SELECT "" AS "memberUuid", "memberEmail", "createdAt", "id", "orgId", "request", "response", "source", "statusCode" FROM "IntegrationUsageLog2";
 
--- UPDATE IntegrationUsageLog
--- SET
---   memberUuid = (
---     SELECT
---       m.uuid
---     FROM
---       Member m
---     WHERE
---       m.orgId = IntegrationUsageLog.orgId
---       AND m.email = IntegrationUsageLog.memberEmail
---     LIMIT
---       1
---   );
--- ALTER TABLE IntegrationUsageLog DROP COLUMN memberEmail;
+UPDATE IntegrationUsageLog
+SET
+  memberUuid = (
+    SELECT
+      m.uuid
+    FROM
+      Member m
+    WHERE
+      m.orgId = IntegrationUsageLog.orgId
+      AND m.email = IntegrationUsageLog.memberEmail
+    LIMIT
+      1
+  );
+ALTER TABLE IntegrationUsageLog DROP COLUMN memberEmail;
 
--- ALTER TABLE MemberKey RENAME TO MemberKey2;
--- CREATE TABLE "MemberKey" (
---     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
---     "keyLabel" TEXT,
---     "keyType" TEXT NOT NULL,
---     "memberUuid" TEXT NOT NULL,
---     "memberEmail" TEXT NOT NULL,
---     "secret" TEXT NOT NULL,
---     "suspend" INTEGER NOT NULL DEFAULT 0
--- );
--- INSERT INTO "MemberKey" ("memberUuid", "memberEmail", "id", "keyLabel", "keyType", "secret", "suspend") SELECT "" AS "memberUuid", "memberEmail", "id", "keyLabel", "keyType", "secret", "suspend" FROM "MemberKey2";
--- UPDATE MemberKey
--- SET
---   memberUuid = (
---     SELECT
---       m.uuid
---     FROM
---       Member m
---     WHERE
---       m.email = MemberKey.memberEmail
---     LIMIT
---       1
---   );
--- ALTER TABLE MemberKey DROP COLUMN memberEmail;
--- CREATE UNIQUE INDEX "MemberKey_memberUuid_secret_key" ON "MemberKey"("memberUuid", "secret");
+ALTER TABLE MemberKey RENAME TO MemberKey2;
+CREATE TABLE "MemberKey" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "keyLabel" TEXT,
+    "keyType" TEXT NOT NULL,
+    "memberUuid" TEXT NOT NULL,
+    "memberEmail" TEXT NOT NULL,
+    "secret" TEXT NOT NULL,
+    "suspend" INTEGER NOT NULL DEFAULT 0
+);
+INSERT INTO "MemberKey" ("memberUuid", "memberEmail", "id", "keyLabel", "keyType", "secret", "suspend") SELECT "" AS "memberUuid", "memberEmail", "id", "keyLabel", "keyType", "secret", "suspend" FROM "MemberKey2";
+UPDATE MemberKey
+SET
+  memberUuid = (
+    SELECT
+      m.uuid
+    FROM
+      Member m
+    WHERE
+      m.email = MemberKey.memberEmail
+    LIMIT
+      1
+  );
+ALTER TABLE MemberKey DROP COLUMN memberEmail;
+CREATE UNIQUE INDEX "MemberKey_memberUuid_secret_key" ON "MemberKey"("memberUuid", "secret");
 
--- ALTER TABLE `Session` RENAME TO Session2;
--- CREATE TABLE "Session" (
---     "kid" TEXT NOT NULL PRIMARY KEY,
---     "orgId" TEXT NOT NULL,
---     "memberUuid" TEXT NOT NULL,
---     "memberEmail" TEXT NOT NULL,
---     "expiry" INTEGER NOT NULL,
---     "issued" INTEGER NOT NULL,
---     "secret" TEXT,
---     "authn_ip" TEXT,
---     "authn_ua" TEXT
--- );
--- INSERT INTO "Session" ("memberUuid", "memberEmail", "authn_ip", "authn_ua", "expiry", "issued", "kid", "orgId", "secret") SELECT "" AS "memberUuid", "memberEmail", "authn_ip", "authn_ua", "expiry", "issued", "kid", "orgId", "secret" FROM "Session2";
--- UPDATE `Session`
--- SET
---   memberUuid = (
---     SELECT
---       m.uuid
---     FROM
---       Member m
---     WHERE
---       m.orgId = `Session`.orgId
---       AND m.email = `Session`.memberEmail
---     LIMIT
---       1
---   );
--- ALTER TABLE `Session` DROP COLUMN memberEmail;
+ALTER TABLE `Session` RENAME TO Session2;
+CREATE TABLE "Session" (
+    "kid" TEXT NOT NULL PRIMARY KEY,
+    "orgId" TEXT NOT NULL,
+    "memberUuid" TEXT NOT NULL,
+    "memberEmail" TEXT NOT NULL,
+    "expiry" INTEGER NOT NULL,
+    "issued" INTEGER NOT NULL,
+    "secret" TEXT,
+    "authn_ip" TEXT,
+    "authn_ua" TEXT
+);
+INSERT INTO "Session" ("memberUuid", "memberEmail", "authn_ip", "authn_ua", "expiry", "issued", "kid", "orgId", "secret") SELECT "" AS "memberUuid", "memberEmail", "authn_ip", "authn_ua", "expiry", "issued", "kid", "orgId", "secret" FROM "Session2";
+UPDATE `Session`
+SET
+  memberUuid = (
+    SELECT
+      m.uuid
+    FROM
+      Member m
+    WHERE
+      m.orgId = `Session`.orgId
+      AND m.email = `Session`.memberEmail
+    LIMIT
+      1
+  );
+ALTER TABLE `Session` DROP COLUMN memberEmail;
 
 ALTER TABLE Triage RENAME TO Triage2;
 CREATE TABLE "Triage" (
