2787e3e457cdf9f355e66ee1de0fb985893d2d40
Chris Langton <chris@vulnetix.com>
2025-05-12 00:03:37 +1000

    chore: migration missed the group defaults
    Signed-off-by: Chris Langton <chris@vulnetix.com>

--- migrations/0028_group_perms.sql
+++ migrations/0028_group_perms.sql
@@ -9,3 +9,31 @@ CREATE TABLE "MemberGroups" (
 
 CREATE UNIQUE INDEX "Group_name_key" ON "Group"("name");
 CREATE UNIQUE INDEX "MemberGroups_groupUuid_memberUuid_key" ON "MemberGroups"("groupUuid", "memberUuid");
+
+INSERT INTO "Group" ("uuid", "name")
+VALUES
+    ("8ac52122-b9ae-40fb-b4c6-7c83238ae8d4", "Owner"),
+    ("8ac52122-b9ae-40fb-b4c6-7c83238ae8d7", "Audit"),
+    ("8ac52122-b9ae-40fb-b4c6-7c83238ae8d5", "Internal Support"),
+    ("8ac52122-b9ae-40fb-b4c6-7c83238ae8d6", "Internal Admin"),
+    ("8ac52122-b9ae-40fb-b4c6-7c83238ae8d8", "Triage");
+
+INSERT INTO "MemberGroups" ("groupUuid", "memberUuid")
+SELECT 
+    "8ac52122-b9ae-40fb-b4c6-7c83238ae8d4" as "groupUuid",
+    m."uuid" as "memberUuid"
+FROM (
+    SELECT 
+        "uuid", 
+        "orgId", 
+        "createdAt",
+        ROW_NUMBER() OVER (PARTITION BY "orgId" ORDER BY "createdAt" ASC) as row_num
+    FROM "Member"
+) m
+WHERE m.row_num = 1
+AND NOT EXISTS (
+    SELECT 1 
+    FROM "MemberGroups" mg 
+    WHERE mg."memberUuid" = m."uuid" 
+    AND mg."groupUuid" = "8ac52122-b9ae-40fb-b4c6-7c83238ae8d4"
+);
