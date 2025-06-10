cf6afcfa181a97a05831b058ed80ef3963fcf16e
Christopher Langton <chris@langton.cloud>
2025-06-11 02:01:37 +1000

    fix: many bugs

--- migrations/0033_queue_message.sql
+++ migrations/0033_queue_message.sql
@@ -1,9 +1,10 @@
 CREATE TABLE IF NOT EXISTS `QueueMessage` (
     `messageId` TEXT PRIMARY KEY NOT NULL,
     `status` TEXT NOT NULL,
-    `uniqueId` TEXT NOT NULL,
-    `linkId` TEXT NOT NULL,
+    `uniqueId` TEXT,
+    `linkId` TEXT,
     `type` TEXT NOT NULL,
+    `displayText` TEXT NOT NULL,
     `orgId` TEXT NOT NULL,
     `createdAt` INTEGER NOT NULL
 );
